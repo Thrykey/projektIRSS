cefaa7166e57b8bc069af882d68f34c5c9a47f9e
0x024e <exo@onet.eu>
2026-02-11 02:58:06 +0100

    typo

--- src/Utils.js
+++ src/Utils.js
@@ -168,7 +168,7 @@ export async function getMe() {
     //     return JSON.parse(cached)
     // }
 
-    const me = await fetch(APIUrl + "/user/me", {
+    const me = await fetch(APIUrl + "/users/me", {
         method: "GET",
         credentials: "include"
     })
